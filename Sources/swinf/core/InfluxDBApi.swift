7b9b1cd73dec689ed149ba6911f07c2f5332e3d4
Martin Grüner <martin.gruener@twinformatics.at>
2019-09-21 19:08:20 +0200

    added basic auth

--- Sources/swinf/core/InfluxDBApi.swift
+++ Sources/swinf/core/InfluxDBApi.swift
@@ -9,18 +9,18 @@ import Foundation
 import swift_net
 
 public enum InfluxAPI {
-    case createDatabase(url: URL, name: String)
-    case write(url: URL, dbName: String, measure: String)
-    case query(url: URL, dbName: String, q: String)
+    case createDatabase(url: URL, name: String, user: String?, pw: String?)
+    case write(url: URL, dbName: String, measure: String, user: String?, pw: String?)
+    case query(url: URL, dbName: String, q: String, user: String?, pw: String?)
 }
 
 extension InfluxAPI: TargetType {
     
     public var baseURL: URL {
         switch self {
-        case .createDatabase(let url, _): return url
-        case .write(let url, _, _): return url
-        case .query(let url, _, _): return url
+        case .createDatabase(let url, _, _, _): return url
+        case .write(let url, _, _, _, _): return url
+        case .query(let url, _, _, _, _): return url
             
         }
     }
@@ -42,18 +42,23 @@ extension InfluxAPI: TargetType {
 
     public var task: Task {
         switch self {
-        case .createDatabase(_ , let name):
+        case .createDatabase(_ , let name, _, _):
             return .requestParameters(parameters: ["q": "CREATE DATABASE \(name)"], encoding: URLEncoding.queryString)
-        case .write(_, let dbName, let measure):
+        case .write(_, let dbName, let measure, _, _):
             return .requestCompositeData(bodyData: measure.data(using: .utf8)!, urlParameters: ["db": dbName])
-        case .query(_ , let dbName, let query):
+        case .query(_ , let dbName, let query, _, _):
             return .requestParameters(parameters: ["db": dbName, "q": query], encoding: URLEncoding.queryString)
         
         }
     }
     
     public var headers: [String: String]? {
-        return [:]
+        switch self {
+        case .createDatabase(_, _, let user, let pw), .query(_, _, _, let user,let pw), .write(_, _, _, let user, let pw):
+            guard let user = user, let pw = pw else { return [:] }
+            let authHeader = HTTPHeader.authorization(username: user, password: pw)
+            return [authHeader.name: authHeader.value]
+        }
     }
     
     public var sampleData: Data {
